6945134888b99960f28499222244e76a8210a956
Ibraim Asanov <iasanov@nemetschek.bg>
2022-01-26 20:37:21 +0200

    Bring back comments

--- src/main/java/automation/qa/MyFirstJava.java
+++ src/main/java/automation/qa/MyFirstJava.java
@@ -14,18 +14,18 @@ public class MyFirstJava {
         //a string variable that will store the value entered from the user
         String firstName = scanner.next();
 
-        //System.out.println("Your name is: " + firstName);
+        System.out.println("Your name is: " + firstName);
 
         System.out.println("please enter your age:");
 
         //integer variable that will store the age entered from the user
         int userAge = scanner.nextInt();
 
-        /* System.out.println("Your name is: " + firstName +
-                "\nYour age is: " + userAge); */
+        System.out.println("Your name is: " + firstName +
+                "\nYour age is: " + userAge);
 
-        //System.out.println("Your name is: " + firstName);
-        //System.out.println("Your age is: " + userAge);
+        System.out.println("Your name is: " + firstName);
+        System.out.println("Your age is: " + userAge);
 
         if (userAge >= 18){
             System.out.println(firstName + " can buy alcohol legally");
